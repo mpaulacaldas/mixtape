815c678e22aaceba303e5caf93c2232e9b1107ce
Maria Paula Caldas <mpaulacaldas@gmail.com>
2019-06-17 09:57:05 -0500

    Add links

--- R/data.R
+++ R/data.R
@@ -1,100 +1,120 @@
 #' Acemoglu, et al. (2001) "The Colonial Origins of Comparative Development: An Empirical Investigation", AER, 91(5) December, pp. 1369-1401.
 #'
+#' @source \url{http://economics.mit.edu/files/4123}
 "acemogluetal"
 
 
 #' Albouy, David Y. (2012) "The Colonial Origins of Comparative Development: An Empirical Investigation: Comment", AER, 102(6), pp. 3059-3076
 #'
+#' @source \url{https://pdfs.semanticscholar.org/f179/00569cfd18339b3dbc53e66e2718b5db4a9c.pdf}
 "ajrcomment"
 
 
 #' Abadie, Alberto and Javier Gardeazabal (2003) "The Economic Costs of Conflict: A Case Study of the Basque Country", AER, 93(1), March, pp. 113-132
 #'
+#' @source \url{http://www.nyu.edu/gsas/dept/politics/faculty/beck/abadie_aer.pdf}
 "basque"
 
 
 #' Card, David (1995) "Using Geographic Variation in College Proximity to Estimate the Return to Schooling", NBER Working Paper No. 4483
 #'
+#' @source \url{https://www.dropbox.com/s/31xrncpk6gmlns3/Card%201995.pdf?dl=0}
 "card"
 
 
 #' Cheng, Cheng and Mark Hoekstra (2013) "Does Strengthening Self-Defense Law Deter Crime or Escalate Violence? Evidence from Expansions to Castle Doctrine" 48(3) pp. 821-854.
 #'
+#' @source \url{http://jhr.uwpress.org/content/48/3/821.abstract}
 "castle-doctrine-2000-2010"
 
 
 #' Cornwell, Christopher and William N. Trumbull (1994) "Estimating the Economic Model of Crime with Panel Data", Review of Economics and Statistics 76(2) May, 360-366.
 #'
+#' \@source \url{https://www.amherst.edu/system/files/media/1468/CornwellTrumbullCrime%2520Elasticities.pdf}
 "cornwell"
 
 
 #' Dehejia, Rajeev H. and Sadek Wahba (2002) "Propensity Score-Matching Methods for Nonexperimental Causal Studies" Review of Economics and Statistics 84(1) February, 151-161.
 #'
+#' \@source \url{http://www.uh.edu/~adkugler/Dehejia&Wahba.pdf}
 "cps_controls"
 
 
 #' Black, Dan A., Terra G. McKinnish and Seth G. Sanders (2003) "Does the Availability oof High-wage Jobs from Low-skilled Men Affect Welfare Expenditures? Evidence from Shocks to the Steel and Coal Industries", Journal of Public Economics, 87, 1921-1942
 #'
+#' @source \url{https://www.dropbox.com/s/b1wn1qxbvfqmfd8/coalIV.dta?dl=0}
 "coalIV"
 
 
 #' Graddy, Kathryn "The Fulton Fish Market" Journal of Economic Perspectives, 20(2), Spring, 207-220. 1
 #'
+#' @source \url{https://www.dropbox.com/s/4ab4vgxeskc94cq/Graddy%202006.pdf?dl=0}
 "fish"
 
 
 #' LaLonde, Robert J. "Evaluating the Econometric Evaluations of Training Programs with Experimental Data" AER, 76(4) September, 604-620. 1
 #'
+#' @source \url{http://people.hbs.edu/nashraf/LaLonde_1986.pdf}
 "lalonde_1986"
 
 
 #' UCR and LEOKA files, 1960-2005, from FBI 1
 #'
+#' @source \url{http://eml.berkeley.edu/~jmccrary/UCR/index.html}
 "leoka2004"
 
 
 #' UCR and LEOKA files, 1960-2005, from FBI 1
 #'
+#' @source \url{http://eml.berkeley.edu/~jmccrary/UCR/index.html}
 "leoka2005"
 
 
 #' UCR and LEOKA files, 1960-2005, from FBI 1
 #'
+#' @source \url{http://eml.berkeley.edu/~jmccrary/UCR/index.html}
 "ucr2004"
 
 
 #' UCR and LEOKA files, 1960-2005, from FBI 1
 #'
+#' @source \url{http://eml.berkeley.edu/~jmccrary/UCR/index.html}
 "ucr2005"
 
 
 #' Affect-or-Elect-Policies-Evidence-from Lee, David S., Enrico Moretti, and Matthew J. Butler (2004), "Do Voters Affect or Elect Policies? Evidence from the U.S. House", August, 119(3), 807-859. 1
 #'
+#' @source \url{https://academic.oup.com/qje/article/119/3/807/1938834/Do-Voters-Affect-or-Elect-Policies-Evidence-from}
 "lmb-data"
 
 
 #' Beland, Louis-Phillippe (2015) "Political Parties and Labor-Market Outcomes: Evidence from US States" AEJ: Applied, 7(4) October, 198-220. 1
 #'
+#' @source \url{https://www.aeaweb.org/articles?id=10.1257/app.20120387}
 "margin_victory"
 
 
 #' Dehejia, Rajeev H. and Sadek Wahba (2002) "Propensity Score-Matching Methods for Nonexperimental Causal Studies" Review of Economics and Statistics 84(1) February, 151-161. 1
 #'
+#' @source \url{http://www.uh.edu/~adkugler/Dehejia&Wahba.pdf}
 "nsw_dw"
 
 
 #' Dehejia, Rajeev H. and Sadek Wahba (2002) "Propensity Score-Matching Methods for Nonexperimental Causal Studies" Review of Economics and Statistics 84(1) February, 151-161. 1
 #'
+#' @source \url{http://www.uh.edu/~adkugler/Dehejia&Wahba.pdf}
 "psid_controls"
 
 
 #' Abadie, Alberto, Alexis Diamond and Jens Hainmueller (2010) "Synthetic Control Methods for Comparative Case Studies: Estimating the Effect of California's Tobacco Control Program" Journal of the American Statistical Association, 105(490), June, 493-505. 1
 #'
+#' @source \url{http://economics.mit.edu/files/11859}
 "smoking"
 
 
 #' Estimates-of-Education-Production Krueger, Alan B. (1999) "Experiental Estimates of Education Production Functions", Quarterly Journal of Economics 114(2) May, 497-532. 1
 #'
+#' @source \url{https://academic.oup.com/qje/article/114/2/497/1844226/Experimental-Estimates-of-Education-Production}
 "star_sw"
 
 
@@ -105,11 +125,13 @@
 
 #' Titanic manifest, both survivors and those who perished along several dimensions (e.g., sex, age, passenger class) 2
 #'
+#' @source \url{https://www.dropbox.com/s/fxuph22p88vkacc/titanic3_readme.pdf?dl=0}
 "titanic"
 
 
 #' Titanic manifest, both survivors and those who perished along several dimensions (e.g., sex, age, passenger class) 2
 #'
+#' @source \url{https://www.dropbox.com/s/fxuph22p88vkacc/titanic3_readme.pdf?dl=0}
 "titanic3"
 
 
